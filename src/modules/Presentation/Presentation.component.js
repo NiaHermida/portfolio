c74cc99c554dbb923899c9f474da5da331552107
EstefaníaH <tef.hermida@gmail.com>
2025-07-16 21:59:56 -0300

    added the profile picture

--- src/modules/Presentation/Presentation.component.js
+++ src/modules/Presentation/Presentation.component.js
@@ -1,6 +1,28 @@
+import Image from "next/image";
 import "./Presentation.scss";
 
 const Presentation = () => {
-  return <div className="PortfolioPresentation">Presentation hi</div>;
+  return (
+    <div className="PortfolioPresentation">
+      <div className="PortfolioPresentation__ImageContainer">
+        <Image
+        className="PortfolioPresentation__Image"
+          src="/profile.png"
+          alt="profile picture"
+       
+          width="550"
+          height="550"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          priority
+        />
+      </div>
+      <h1>Hola, soy Estefanía Hermida</h1>
+      <h2>Full Stack Developer & UI/UX entusiasta</h2>
+      <h3>
+        Creo páginas web dinámicas y responsive con JS y React. Me apasiona el
+        código limpio, la experiencia de usuario y aprender constantemente.
+      </h3>
+    </div>
+  );
 };
 export default Presentation;
